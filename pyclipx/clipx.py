9a4c55a4be87e686539ca50e5907b625886cd9d9
Oliver Lindemann <lindemann09@googlemail.com>
2026-07-23 10:40:23 +0200

    v0.1

--- pyclipx/clipx.py
+++ pyclipx/clipx.py
@@ -1,18 +1,16 @@
-from clipx_api import ClipXAPI
-from time import sleep
+from clipx_api import ClipXAPI, get_signal_id, SIGNAL_LABELS
+from time import sleep, time
 ip = '10.144.71.141'
+v_id = get_signal_id("Field Value")
 clipx = ClipXAPI()
 
 clipx.connect(ip)
 if clipx.is_connected():
-
+    t_start = time()
     clipx.start_measurement()
-    for x in range(100):
-        for _ in range(clipx.available_lines()):
-            x = clipx.read_next_line()
-            print("---", x)
-        else:
-            sleep(0.1)
+    while time()-t_start<5:
+        for d in clipx.read_available_blocks():
+            print(f"{d.time}, {d.values[v_id]}")
     clipx.stop_measurement()
     clipx.disconnect()
 
