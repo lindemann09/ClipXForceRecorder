8a9a480c52eb91c17d061455a6f25a6fc90c68e2
Oliver Lindemann <lindemann@essb.eur.nl>
2026-08-01 16:53:14 +0200

    add logging

--- src/clipx_force_recorder/__init__.py
+++ src/clipx_force_recorder/__init__.py
@@ -4,7 +4,11 @@ Recording and streaming ClipX Force Sensors with Python and Lab Streaming Layer 
 
 from importlib.metadata import version
 
-__version__ = version("clipx_force_recorder")
+from .misc import set_logging as _set_logging
+
+APPNAME = "clipx_force_recorder"
+__version__ = version(APPNAME)
 __author__ = "Oliver Lindemann"
 
+LOGFILE = _set_logging(log_file=f"{APPNAME}.log") # TODO: logging
 
