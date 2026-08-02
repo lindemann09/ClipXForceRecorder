b9bb43c6f834fd00b86ea60b2480f291a4705ebe
Oliver Lindemann <lindemann09@users.noreply.github.com>
2026-08-02 23:37:18 +0200

    Aktualisieren von data.py

--- src/clipx_force_recorder/tools/data.py
+++ src/clipx_force_recorder/tools/data.py
@@ -1,23 +1,10 @@
-import logging
 from collections import deque
 from pathlib import Path
 from time import perf_counter
 
-import appdirs
 import numpy as np
-from numpy.typing import NDArray
+from numpy.typing import NDAarry
 
-
-def set_logging(log_file):
-    log_dir = Path(appdirs.AppDirs("").user_log_dir)
-    log_dir.mkdir(parents=True, exist_ok=True)
-    log_file = log_dir / log_file
-    logging.basicConfig(filename=log_file,
-                        encoding='utf-8',
-                        format="[%(asctime)s] %(levelname)s: %(message)s",
-                        datefmt="%m%d %H:%M:%S",
-                        level=logging.INFO)
-    return log_file
 
 def N2g(N):
     kg = N / 9.81
